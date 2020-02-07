c9b92143b271562b5db3a726a53075df7e3dccec
bactvk <tikmtute@gmail.com>
2020-02-07 16:01:27 +0700

    version 1.4

--- hajimari/public/assets/js/script.js
+++ hajimari/public/assets/js/script.js
@@ -6,92 +6,9 @@ $(document).ready(function(){
 			$('input.place_work_search').siblings().removeClass("show");
 		}else $(this).siblings().addClass("show");	
 
-	})
-	
-
-	var city = '',field='',salary_level='',languge_level='';
-	var total_checked_workplace = 0 ,total_checked_field_search = 0 ,total_checked_field_search_salary = 0 ,total_checked_field_search_languge = 0;
-	$('body').on('click','.workplace',function(){
-
-		if($(this).prop('checked')){
-			total_checked_workplace += 1;
-			city += $(this).parent().text()+',';
-		}else{
-			total_checked_workplace -= 1;
-			city = city.replace($(this).parent().text()+',','');
-		}
-		if(total_checked_workplace > 3){
-			$('input.place_work_search').siblings().removeClass("show");
-
-			$(this).parent().parent().parent().html('<li style="padding:8px 14px;border:1px solid">You can only select 3 items</li>');
-		}else{
-			$(this).parent().parent().parent().siblings('.place_work_search').attr('value',city.replace(/,\s*$/, ""));
-		}
-		
-	})
-
-	$('body').on('click','.field_search',function(){
-
-		if($(this).prop('checked')){
-			total_checked_field_search += 1;
-			field += $(this).parent().text()+',';
-		}else{
-			total_checked_field_search -= 1;
-			field = field.replace($(this).parent().text()+',','');
-		}
-
-		if(total_checked_field_search > 3){
-			$('input.place_work_search').siblings().removeClass("show");
-
-			$(this).parent().parent().parent().html('<li style="padding:8px 14px;border:1px solid">You can only select 3 items</li>');
-		}else{
-			$(this).parent().parent().parent().siblings('.place_work_search').attr('value',field.replace(/,\s*$/, ""));
-		}
-		
-	})
-
-	$('body').on('click','.field_search_salary',function(){
-
-		if($(this).prop('checked')){ 
-			total_checked_field_search_salary += 1;
-			salary_level += $(this).parent().text()+',';
-			
-		}else{
-			total_checked_field_search_salary -= 1;
-			salary_level = salary_level.replace($(this).parent().text()+',','');
-		}
-
-		if(total_checked_field_search_salary > 3){
-			$('input.place_work_search').siblings().removeClass("show");
-
-			$(this).parent().parent().parent().html('<li style="padding:8px 14px;border:1px solid">You can only select 3 items</li>');
-		}else{
-			$(this).parent().parent().parent().siblings('.place_work_search').attr('value',salary_level.replace(/,\s*$/, ""));
-		}
-	})
-
-	$('body').on('click','.field_search_languge',function(){ 
-
-		if($(this).prop('checked')){
-			total_checked_field_search_languge += 1;
-			languge_level += $(this).parent().text()+',';
-		}else{
-			total_checked_field_search_languge -= 1;
-			languge_level = languge_level.replace($(this).parent().text()+',','');
-		}
-
-		if(total_checked_field_search_languge > 3){
-			$('input.place_work_search').siblings().removeClass("show");
-
-			$(this).parent().parent().parent().html('<li style="padding:8px 14px;border:1px solid">You can only select 3 items</li>');
-		}else{
-			$(this).parent().parent().parent().siblings('.place_work_search').attr('value',languge_level.replace(/,\s*$/, ""));
-		}	
-	})
-
-
+	})	
 	// select2
-	$(".js-example-basic-multiple").select2({
+	$(".workplace").select2({
 	    placeholder: "Địa điểm làm việc",
 	    maximumSelectionLength: 3,
 	    closeOnSelect: false
@@ -102,9 +19,6 @@ $(document).ready(function(){
 	//     maximumSelectionLength: 3,
 	//     closeOnSelect: false
 	// });
-
-
-
 
 	$(".laguageLevel").select2({
 	    placeholder: "Trình độ tiếng nhật",
@@ -117,18 +31,6 @@ $(document).ready(function(){
 	    maximumSelectionLength: 3,
 	    closeOnSelect: false
 	});
-
-	
-
-	// $(".js-example-basic-multiple").on("select2:select", function (e) { 
-	//   var select_val = $(e.currentTarget).val();
-	//   $(this).siblings().val(select_val);
-	//   console.log(select_val)
-	// });
-
-	
-	// 
-
 
 
 	$(function() {
--- hajimari/resources/views/find_job/_form_search_job.blade.php
+++ hajimari/resources/views/find_job/_form_search_job.blade.php
@@ -19,7 +19,7 @@
   </div> --}}
 
   <div class="col-12 col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-0">
-    <select name="workplace[]" class="form-control form-control-lg js-example-basic-multiple" multiple="multiple">
+    <select name="workplace[]" class="form-control form-control-lg workplace" multiple="multiple">
         <option></option>
         @foreach($listWorkplace as $city)
           
